c1672596a69891c759064a17273ceadf388f2c16
rmercas <rodolfomercas@gmail.com>
2021-08-04 23:56:48 -0500

    version 1

---
+++ reactcart/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import * as ReactBootStrap from "react-bootstrap";
+function App() {
+  return (
+    <div className="App">
+     <ReactBootStrap.Navbar bg="dark" variant="dark">
+    <ReactBootStrap.Container>
+    <ReactBootStrap.Navbar.Brand href="#home">Navbar</ReactBootStrap.Navbar.Brand>
+    <ReactBootStrap.Nav className="me-auto">
+      <ReactBootStrap.Nav.Link href="#home">Home</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.Nav.Link href="#features">Features</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.Nav.Link href="#pricing">Pricing</ReactBootStrap.Nav.Link>
+    </ReactBootStrap.Nav>
+    </ReactBootStrap.Container>
+  </ReactBootStrap.Navbar>
+
+    </div>
+  );
+}
+
+export default App;
